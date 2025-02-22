796010aab39a383dda737ec4652b56f1ae2f3861
biswaranjandas <akashranjan169@gmail.com>
2025-02-22 18:16:07 +0530

    Added the first TDD test case.

---
+++ src/test/java/com/incubyte/tdd/AdditionTest.java
@@ -0,0 +1,14 @@
+package com.incubyte.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AdditionTest {
+
+    @Test
+    public void testAddEmptyString() {
+        StringAddition stringAddition = new StringAddition();
+
+    }
+}
